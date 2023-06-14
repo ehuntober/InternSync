276de9a9fdf36c7a7a5a0cc0b75efbfcafafc2ec
ehuntober <ehuntercrypto@gmail.com>
2023-06-14 16:03:37 +0000

    all internship routes created

--- controllers/internshipController.js
+++ controllers/internshipController.js
@@ -1,16 +1,15 @@
 
-const Intership = require('../models/internship');
+const Internship = require('../models/internship');
 
-exports.getAll = (req,res) =>{
-    Intership.find()
-    .then(Interships =>{
-        res.json(Intership)
-    })
-
-    .catch(err=>{
-        res.status(500).json({error: 'An error occurred'})
-    });
-};
+exports.getAll = (req, res) => {
+    Internship.find()
+      .then(internships => {
+        res.json(internships);
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'An error occurred' });
+      });
+  };
 
 
 exports.create = (req,res) =>{
@@ -27,3 +26,51 @@ exports.create = (req,res) =>{
     })
 
 };
+
+
+exports.getById = (req,res) => {
+    const internshipId = req.params.id;
+    Internship.findById(internshipId)
+    .then(internship =>{
+        if (!internshp){
+            return res.status(404).json({err: 'Internship not found'});
+
+        }
+        res.json(internship);
+    })
+    .catch(err =>{
+        res.status(500).json({error: 'An error occurred'})
+    });
+}
+
+
+// Update an existing internship
+exports.update = (req, res) => {
+    const internshipId = req.params.id;
+    const updatedData = req.body;
+    Internship.findByIdAndUpdate(internshipId, updatedData, { new: true })
+      .then(internship => {
+        if (!internship) {
+          return res.status(404).json({ error: 'Internship not found' });
+        }
+        res.json(internship);
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'An error occurred' });
+      });
+  };
+  
+  // Delete an internship
+  exports.delete = (req, res) => {
+    const internshipId = req.params.id;
+    Internship.findByIdAndDelete(internshipId)
+      .then(internship => {
+        if (!internship) {
+          return res.status(404).json({ error: 'Internship not found' });
+        }
+        res.sendStatus(204);
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'An error occurred' });
+      });
+  };
--- routes/internships.js
+++ routes/internships.js
@@ -6,9 +6,9 @@ const internshipController = require('../controllers/internshipController');
 
 // routes for internships
 router.get('/', internshipController.getAll);
-// router.get('/:id', internshipController.getById);
+router.get('/:id', internshipController.getById);
 router.post('/',internshipController.create)
-// router.put('/:id',internshipController.update);
-// router.delete('/:id',internshipController.delete);
+router.put('/:id',internshipController.update);
+router.delete('/:id',internshipController.delete);
 
 module.exports = router;
