6ba3fc841dde202295ea225f7ca8d9b6af2e7c37
ehuntober <ehuntercrypto@gmail.com>
2023-08-11 14:32:51 +0000

    new file added

--- src/routes/internRoutes.js
+++ src/routes/internRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
+const internAuthMiddleware = require('../middleware/internAuthMiddleware');
 const internController = require('../controllers/internController');
 
 // Auth middleware to ensure only authenticated interns can access their profiles
-router.use(authMiddleware);
+router.use(internAuthMiddleware);
 
 router.put('/:id', internController.updateInternProfile);
 
