9c9e4e1c7f254f5f396a31218399b9f041a0c15d
ehuntober <ehuntercrypto@gmail.com>
2023-08-12 13:41:49 +0000

    new file added

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
-const authMiddleware = require('../middleware/authMiddleware');
+
 
 const adminAuthMiddleware = require('../middleware/adminAuthMiddleware');
 
