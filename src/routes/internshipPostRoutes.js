09fe4379625b7266926fd113fd869446770986d5
ehuntober <ehuntercrypto@gmail.com>
2023-08-12 13:40:53 +0000

    new file added

--- src/routes/internshipPostRoutes.js
+++ src/routes/internshipPostRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
+const organizationAuthMiddleware = require('../middleware/organizationAuthMiddleware');
 const internshipPostController = require('../controllers/internshipPostController');
 
 // Auth middleware to ensure only authenticated organizations can create posts
-router.use(authMiddleware);
+router.use(organizationAuthMiddleware);
 
 router.post('/:id/posts', internshipPostController.createInternshipPost);
 
